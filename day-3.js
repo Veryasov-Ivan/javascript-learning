e1d3a160ad14fe35ab673108154424101d45ac27
veryasov <ivanveryasov9@gmail.com>
2026-06-14 21:07:08 +0700

    Add Day 3: variables and console.log practice

---
+++ day-3.js
@@ -0,0 +1,13 @@
+// Day 3
+
+const botVersion = "1.0.0";
+
+let activeUsers = 49;
+
+activeUsers = 50;
+
+console.log("Bot version:");
+console.log(botVersion);
+
+console.log("Active users:");
+console.log(activeUsers);
