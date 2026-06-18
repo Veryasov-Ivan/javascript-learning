253298118d206fda4fa7bb728d7e81369e8dee63
veryasov <ivanveryasov9@gmail.com>
2026-06-18 23:16:15 +0700

    Add Day 7: AND and OR operator practice

---
+++ day-7.js
@@ -0,0 +1,11 @@
+// Day 7
+
+const followers = 15000;
+const isTechNiche = true;
+const hasBadReputation = false;
+
+if (followers > 10000 && isTechNiche === true && hasBadReputation === false) {
+    console.log("The influencer is a good fit! Sending a collaboration offer.");
+} else {
+    console.log("The influencer does not meet the campaign requirements.");
+}
