e1f3f586998974d82f8271a56cd9ed5953935578
veryasov <ivanveryasov9@gmail.com>
2026-06-21 18:34:39 +0700

    Add Day 10: playlist array practice

--- day-10.js
+++ day-10.js
@@ -1,5 +1,10 @@
-// Day 10 // R.I.P Oliver Tree
+// Day 10 
+// R.I.P Oliver Tree
 
-const playlist = ["Life Goes On", "Miss You", "Hurt"];
+const playlist = ["Life Goes On", "000-000-000", "Hurt"];
 
-console.log(playlist[index]);
+console.log(playlist[1])
+
+const index = 0;
+
+console.log(playlist[index])
